11368b46bdaf0b41e6a025e94d70dc618ee300c7
eric <1254122559@qq.com>
2024-04-09 23:43:53 +0800

    基本实现设定的对 https请求的捕获

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module proxy-http
 
 go 1.20
+
+require (
+	github.com/elazarl/goproxy v0.0.0-20231117061959-7cc037d33fb5 // indirect
+	github.com/kxg3030/shermie-proxy v1.1.1 // indirect
+	github.com/viki-org/dnscache v0.0.0-20130720023526-c70c1f23c5d8 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,104 +1,175 @@
 package main
 
 import (
+	"bytes"
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"mime"
 	"net/http"
-	"net/url"
+	"os"
+	"strings"
+
+	"github.com/elazarl/goproxy"
 )
 
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-
-	fmt.Printf("Received request: %s %s\n", r.Method, r.URL)
-	//for name, values := range r.Header {
-	//	for _, value := range values {
-	//		fmt.Printf("%s: %s\n", name, value)
-	//	}
-	//}
-
-	contentType, _, _ := mime.ParseMediaType(r.Header.Get("Content-Type"))
-
-	if r.Method == "GET" {
-		queryParams := r.URL.Query()
-		for name, values := range queryParams {
-			for _, value := range values {
-				fmt.Printf("请求参数: %s = %s\n", name, value)
-			}
-		}
-	} else {
-		switch contentType {
-		case "application/json":
-			var jsonData map[string]interface{}
-			if err := json.NewDecoder(r.Body).Decode(&jsonData); err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
-				return
-			}
-			fmt.Println("JSON 参数:")
-			for key, value := range jsonData {
-				fmt.Printf("  %s: %v\n", key, value)
-			}
-		case "application/x-www-form-urlencoded":
-			if err := r.ParseForm(); err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
-				return
-			}
-			fmt.Println("Form 参数:")
-			for key, values := range r.PostForm {
-				for _, value := range values {
-					fmt.Printf("  %s: %s\n", key, value)
-				}
-			}
-		// Add more cases for other content types as needed
-		default:
-			fmt.Printf("目前不支持的 content type: %s\n", contentType)
-		}
-	}
-
-	// Create a new request to send to the target server
-	targetURL, err := url.Parse(r.URL.String())
-	if err != nil {
-		http.Error(w, "错误 URL", http.StatusBadRequest)
-		fmt.Printf("错误 URL: %v\n", err)
-		return
-	}
-
-	newRequest, err := http.NewRequest(r.Method, targetURL.String(), r.Body)
-	if err != nil {
-		http.Error(w, "请求发生错误", http.StatusInternalServerError)
-		fmt.Printf("错误发送请求: %v\n", err)
-		return
-	}
-
-	copyHeader(newRequest.Header, r.Header)
-
-	client := &http.Client{}
-	resp, err := client.Do(newRequest)
-	if err != nil {
-		http.Error(w, "Server Error", http.StatusInternalServerError)
-		fmt.Printf("Server error: %v\n", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	copyHeader(w.Header(), resp.Header)
-	w.WriteHeader(resp.StatusCode)
-	io.Copy(w, resp.Body)
-}
-
-func copyHeader(dst, src http.Header) {
-	for k, vv := range src {
-		for _, v := range vv {
-			dst.Add(k, v)
-		}
-	}
-}
+var staticExts = []string{".js", ".css", ".png", ".jpg", ".jpeg", ".gif", ".svg", ".woff", ".woff2", ".ttf", ".eot"}
 
 func main() {
-	http.HandleFunc("/", handleRequest)
-	fmt.Println("启动代理服务 在端口:9999")
-	if err := http.ListenAndServe(":9999", nil); err != nil {
-		fmt.Printf("Error starting proxy server: %v\n", err)
+	var recRespDataFlag string
+	var filterResource string
+	var filterOtherHttp string
+	fmt.Println(`------------------  合肥中台 http(s) 抓包工具 -------------------
+$$$$$$$$\ $$$$$$\ $$\   $$\  $$$$$$\  $$$$$$\ $$\   $$\  $$$$$$\  
+$$  _____|\_$$  _|$$$\  $$ |$$  __$$\ \_$$  _|$$$\  $$ |$$  __$$\ 
+$$ |        $$ |  $$$$\ $$ |$$ /  \__|  $$ |  $$$$\ $$ |$$ /  $$ |
+$$$$$\      $$ |  $$ $$\$$ |$$ |        $$ |  $$ $$\$$ |$$$$$$$$ |
+$$  __|     $$ |  $$ \$$$$ |$$ |        $$ |  $$ \$$$$ |$$  __$$ |
+$$ |        $$ |  $$ |\$$$ |$$ |  $$\   $$ |  $$ |\$$$ |$$ |  $$ |
+$$ |      $$$$$$\ $$ | \$$ |\$$$$$$  |$$$$$$\ $$ | \$$ |$$ |  $$ |
+\__|      \______|\__|  \__| \______/ \______|\__|  \__|\__|  \__|		  
+	`)
+
+	fmt.Println("是否 打印接口返回数据（1：是，0：否，默认不打印): 请输入1或0或回车")
+	_, err := fmt.Scanln(&recRespDataFlag)
+	if err != nil {
+		recRespDataFlag = "0"
 	}
+
+	fmt.Println("是否屏蔽非 finchina请求(1：是，0：否，默认屏蔽): 请输入1或0或回车")
+	_, err2 := fmt.Scanln(&filterOtherHttp)
+	if err2 != nil {
+		filterOtherHttp = "1"
+	}
+
+	fmt.Println("是否屏蔽静态资源类请求(1：是，0：否，默认屏蔽): 请输入1或0或回车")
+	_, err1 := fmt.Scanln(&filterResource)
+	if err1 != nil {
+		filterResource = "1"
+	}
+
+	proxy := goproxy.NewProxyHttpServer()
+
+	// 开启详细日志
+	proxy.Verbose = false
+
+	// 设置HTTPS拦截
+	//proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
+	proxy.Tr.Proxy = http.ProxyFromEnvironment
+	proxy.Tr.DialContext = nil
+	proxy.Tr.Dial = nil
+	proxy.Tr.DialTLS = nil
+	proxy.Tr.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	// 创建一个带有过滤器的Logger
+	filteredLogger := log.New(FilteredWriter{w: os.Stderr}, "", log.LstdFlags)
+	// 将goproxy的Logger设置为我们的过滤Logger
+	proxy.Logger = filteredLogger
+
+	// 对CONNECT方法请求动态决定是否执行MITM
+	proxy.OnRequest().HandleConnectFunc(func(host string, ctx *goproxy.ProxyCtx) (*goproxy.ConnectAction, string) {
+		// 检查请求的目标域名
+		if strings.Contains(host, "cdn") {
+			// 对于目标域名包含 cdn 的请求，不执行MITM，直接转发
+			return goproxy.OkConnect, host
+		}
+		// 其他所有请求默认执行MITM
+		return goproxy.MitmConnect, host
+	})
+
+	// 设置请求处理器来修改请求
+	proxy.OnRequest().DoFunc(
+		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+			path := r.URL.String()
+			// 获取请求的路径
+			if strings.Contains(path, ".js") || strings.Contains(path, ".css") || strings.Contains(path, ".html") {
+				r.URL.Host = "appdev.finchina.com"
+				r.Host = "appdev.finchina.com"
+				r.URL.Scheme = "https"
+			}
+			if "1" == filterResource {
+				// 检查路径是否以静态资源的扩展名结尾
+				for _, ext := range staticExts {
+					if strings.HasSuffix(path, ext) {
+						// 是静态资源，不打印日志并直接返回请求
+						return r, nil
+					}
+				}
+			}
+			if "1" == filterOtherHttp && !strings.Contains(path, "finchinaAPP") {
+				return r, nil
+			}
+
+			contentType, _, _ := mime.ParseMediaType(r.Header.Get("Content-Type"))
+			if r.Method == "GET" {
+				log.Printf("接收到 GET请求: %s\n", r.URL.String())
+			} else {
+				log.Printf("接收到 POST请求: %s\n", r.URL.String())
+				switch contentType {
+				case "application/json":
+					var requestBody map[string]interface{}
+					// 读取请求体
+					body, _ := io.ReadAll(r.Body)
+					defer r.Body.Close()
+
+					// 解析JSON
+					_ = json.Unmarshal(body, &requestBody)
+
+					log.Printf("JSON 参数: %v\n", requestBody)
+					r.Body = io.NopCloser(bytes.NewBuffer(body))
+				case "application/x-www-form-urlencoded":
+					fmt.Println("Form 参数如下: ")
+					// 解析请求体中的表单数据
+					if err := r.ParseForm(); err != nil {
+					}
+					// 遍历所有表单参数并打印
+					for key, values := range r.Form {
+						// 因为同一个键可能对应多个值，所以 values 是一个字符串切片
+						for _, value := range values {
+							fmt.Printf("%s = %s\n", key, value)
+						}
+					}
+
+				default:
+					fmt.Printf("目前不支持的 content type: %s\n", contentType)
+				}
+			}
+			return r, nil
+		})
+
+	// 设置响应处理器来修改响应
+	proxy.OnResponse().DoFunc(
+		func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+			if "1" == recRespDataFlag {
+				bodyBytes, err := io.ReadAll(resp.Body)
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer resp.Body.Close() // 关闭Body
+
+				bodyString := string(bodyBytes)
+				// 这里可以根据需要修改resp（响应）
+				log.Printf("接口响应：\n %s\n", bodyString)
+				resp.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+			}
+			return resp
+		})
+
+	// 监听并服务
+	fmt.Println("--------------- 开始监听 本机9999端口 ---------------------")
+	log.Fatal(http.ListenAndServe(":9999", proxy))
+}
+
+type FilteredWriter struct {
+	w io.Writer
+}
+
+func (fw FilteredWriter) Write(p []byte) (n int, err error) {
+	if strings.Contains(string(p), "WARN: Cannot handshake") {
+		// 如果日志消息包含特定字符串，就过滤掉该消息
+		return len(p), nil // 返回成功，但不实际写入
+	}
+	// 否则，将消息写入原始的写入器
+	return fw.w.Write(p)
 }
